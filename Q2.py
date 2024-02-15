cee2a66f2e2bb255b82c16e722d926371a5d1dfe
zero.nabeel@gmail.com <57975428+zero52100@users.noreply.github.com>
2024-02-15 17:46:21 +0530

    .

--- Q2.py
+++ Q2.py
@@ -4,10 +4,19 @@ Q2.Write a Python program to remove duplicates from a list,"""
 """
 2.Write a Python program to remove duplicates from a list,"""
 
-list1=[1,2,4,5,5,6]
+list1=[]
+limit = int(input("Enter number of elements to be insert into to list: "))
+for i in range(limit):
+    list1.append(int(input(f"Enter {(i+1)}th elements : ")))
 list2=[]
-for i in list1:
-    if i not in list2:
-        list2.append(i)
 
-print(list2)
+for i in list1:
+        if i not in list2:
+            list2.append(i)
+
+ 
+
+if len(list1)==len(list2):
+    print("There no duplicate element in the list ")
+else:
+        print(f"List after removing duplicate element {list2}")
--- Q3.py
+++ Q3.py
@@ -2,11 +2,15 @@
 Q3.3.Write a Python program to count the number of strings where the string length is 2 or more.
 	Sample List : ['abc', 'xyz', 'aba', '1']
 	Expected Result : 3"""
-
-List= ['abc', 'xyz', 'abc', '1']
+list=[]
+limit = int(input("Enter number of elements to be insert into to list: "))
+for i in range(limit):
+    list.append((input(f"Enter {(i+1)}th string : ")))
 count=0
-for i in List:
+for i in list:
     if len(i)>2:
         count += 1
-
-print(count)
+if count>0:
+    print(f"Total {count}  string are have lenght {limit} or more limit ")
+else:
+    print(f"All lenght of string in the {list}below 2 ")
